2b9d959af3902d7275b72b11cb48af5b05938fa0
BrianReisman <brianreisman@protonmail.com>
2021-03-08 23:08:53 -0500

    [POST] for /api/users finished

--- api/server.js
+++ api/server.js
@@ -8,40 +8,72 @@ server.use(express.json());
 
 //promise. chaining .then().catch() onto an action invoked.
 server.get("/api/users", async (req, res) => {
-  actions.find()
-    .then(users => {
-      if(!users || users.length === 0){
-        console.log('promise resolved but no data')
-        res.status(400).send({message: 'no users found'}) //!Check status code
+  actions
+    .find()
+    .then((users) => {
+      if (!users || users.length === 0) {
+        res.status(400).send({ message: "no users found" }); //!Check status code
       } else {
-        console.log('users, users, we got some users here!', users)
-        res.status(200).send(users)
+        res.status(200).send(users);
       }
     })
-    .catch(err => {
-      console.log('error in request', err)
-    })
-  // 1- pull info from request <optional; and validate it>
-  // 2- interact with the database
-  // 3- send the client an appropriate response
-
+    .catch((err) => {
+      console.log("error in request", err);
+      res.status(400).send(err);
+    });
 });
-
 
 //async/await needs to be put in a try/catch so give that which is being awaited on, a chance to resolve.
 server.get("/api/users/:id", async (req, res) => {
-  const {id} = req.params;
-  try{
-    const user = await actions.findById(id)
-    console.log(user)
-    res.status(200).send(user)
-  } catch(err){
-    console.log(err)
+  const { id } = req.params;
+  try {
+    const user = await actions.findById(id);
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(400).send(err.message);
   }
 });
 
-// server.post("/api/users", (req, res) => {});
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+  if (
+    typeof name !== "string" ||
+    typeof bio !== "string" ||
+    name.length < 1 ||
+    bio.length < 1
+  ) {
+    res.status(400).json("All inputs are required");
+  } else {
+    actions
+      .insert(req.body)
+      .then((data) => {
+        res.status(200).send(data);
+      })
+      .catch((err) => res.status(400).send(err, "error"));
+  }
+});
+
+// 1- pull info from request <optional; and validate it>
+// 2- interact with the database
+// 3- send the client an appropriate response
+
 // server.delete("/api/users/:id", (req, res) => {});
 // server.put("/api/users/:id", (req, res) => {});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
+
+//!
+//?
+//*
+//todo
+/*
+1. Just like one returns stops a function or loop, it seems some res.methods() act like returns for instance .send() or .json() but not .status(). Is there a way to figure out which to put at the end of the chain? (the 'order of operations') ie. we don't write res.send().status() ever, do we? Is there a name for these types of methods?
+1b. What is the difference between terminating with .send() vs .json()
+2. What is the best way to inspect the request object from within the endpoint. I've tried to return the request object to my postman via res.send() but get the following: "TypeError: Converting circular structure to JSON"
+2b. How do you suggest inspecting the error that comes back from either the .catch in then/catch or from .catch(err) in try/catch. I never know if what I'm looking for is in err or in err.message or otherwise.
+3. What is the best/easier ways to break our promises and try/catches if we want to test the different ways an error might occur? What are the main types of errors we will want to keep in mind to handle?
+*/
+//todo
+//*
+//?
+//!
